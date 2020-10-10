69b3e1243a056d2ba8a8be0f7e00e6d7ff278667
youcando2019 <51888314+youcando2019@users.noreply.github.com>
2020-10-10 17:21:52 +0100

    Update install_online.sh

--- install_online.sh
+++ install_online.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-loadkeys fr-latin1
-wifi-menu
+loadkeys fr
+iwctl
 timedatectl set-ntp true
 
 fdisk -l
@@ -16,39 +16,26 @@ mkfs.ext4 /dev/$partition
 mkswap /dev/$swap
 swapon /dev/$swap
 
-# Variable needed
-# IFS= read -r -> To read spaces characters
-IFS= read -r -p "Create the Password for ROOT : " a
-IFS= read -r -p "Retype Password for ROOT : " b
-if [[ "$a" = "$b" ]]; then
-    pass_root="$a"
-    a="null"
-fi
-read -p "Enter the User Name : " user
-IFS= read -r -p "Create the Password for $user : " a
-IFS= read -r -p "Retype Password for $user : " b
-if [[ "$a" = "$b" ]]; then
-    pass_user="$a"
-    a="null"
-fi
-
 mount /dev/$partition /mnt
 
 # Package configuration
 # Add 32 Bit
-mv /etc/pacman.conf /etc/pacman.conf.old
+cp /etc/pacman.conf /etc/pacman.conf.old
 echo "[multilib]" >> /etc/pacman.conf
 echo "include = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
 
 pacman -Sy --noconfirm
 
-pacstrap /mnt base linux linux-firmware nano sudo bash-completion base-devel git xorg xorg-server grub os-prober intel-ucode amd-ucode lightdm lightdm-deepin-greeter lightdm-gtk-greeter wpa_supplicant networkmanager deepin deepin-extra firefox xterm libreoffice okular vlc yay lib32-libcanberra-pulse
+#pacstrap /mnt base linux linux-firmware nano sudo bash-completion base-devel git xorg xorg-server grub os-prober intel-ucode amd-ucode wpa_supplicant networkmanager blueberry lightdm lightdm-deepin-greeter lightdm-gtk-greeter deepin deepin-extra firefox xterm libreoffice okular vlc yay lib32-libcanberra-pulse
+pacstrap /mnt base linux linux-firmware nano sudo bash-completion base-devel git xorg xorg-server grub os-prober intel-ucode amd-ucode wpa_supplicant networkmanager blueberry gnome gnome-extra firefox libreoffice okular vlc yay lib32-libcanberra-pulse
 genfstab -U /mnt >> /mnt/etc/fstab
 
 cp install_online_2.sh /mnt
 
+echo "Type this command : ./install_online_2.sh"
+
 # Variable needed 1-disk 2-pass_root 3-user 4-pass_user
-arch-chroot /mnt /bin/bash ./install_online_2.sh $disk "$pass_root" "$user" "$pass_user"
+arch-chroot /mnt
 
 rm /mnt/install_online_2.sh
 umount -R /mnt
